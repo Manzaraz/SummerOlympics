9dde0211cfca34f3d33c3e51d3ec3b1061b69dc2
Christian Manzaraz <magnusmanz@icloud.com>
2025-02-01 23:41:57 -0300

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by Christian Manzaraz on 01/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
